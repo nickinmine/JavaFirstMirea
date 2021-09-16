1a6f9daef7b9fc90f1db6194f26f2e44ed3f6e69
Nick <nickinmine@gmail.com>
2021-09-16 15:48:18 +0300

    Добавлены файлы первого, второго подпункта задания

---
+++ src/ru/mirea/task3/book/TestBook.java
@@ -0,0 +1,10 @@
+package ru.mirea.task3.book;
+
+public class TestBook {
+    public static void main(String[] args) {
+        Book b1 = new Book();
+        Book book2 = new Book("Alice in Wonderland");
+        Book book3 = new Book("Pet Sematary", 458, 699.99f);
+
+    }
+}
